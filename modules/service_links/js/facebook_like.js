a42844bf90f09f3279eee102618a7c8743bfe441
Jonathan Hedstrom <jhedstrom@gmail.com>
2011-09-30 14:20:49 -0700

    Updating service links module.

--- modules/service_links/js/facebook_like.js
+++ modules/service_links/js/facebook_like.js
@@ -1,7 +1,11 @@
 (function ($) {
   $(document).ready(function(){
     $('a.service-links-facebook-like').each(function(){
-      iframe_txt='<iframe src="' + $(this).attr('href') + '&amp;layout=' + $(this).attr('layout') + '&amp;show_faces=false&amp;width=' + $(this).attr('width') + '&amp;action='+ $(this).attr('action') + '&amp;colorscheme=' + $(this).attr('colorscheme') + '&amp;height=' + $(this).attr('height') + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' + $(this).attr('width') + 'px; height:' + $(this).attr('height') + 'px;"' + ' allowTransparency="true"></iframe>';
+      var iframe_txt='<iframe src="' + $(this).attr('href').replace('http://', '//') +
+        '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' +
+        Drupal.settings.ws_fl.width + 'px; height:' +
+        Drupal.settings.ws_fl.height + 'px;"' + ' allowTransparency="true"></iframe>';
+
       $(this).replaceWith(iframe_txt);
     });
   });
